08afcb6623207e8be286733f5c2bfd442e266a02
findmelily <pinny.pinngam@gmail.com>
2024-02-17 20:43:23 +0700

    Update Modal

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,22 @@
       <h1 class="text-3xl text-white font-bold mb-4">15 Puzzle Game</h1>
 
       <div v-if="!gameStarted" class="mb-4" @click="playbackgroudSound()">
-        <button @click="startGame('easy')" class="bg-blue-500 text-white py-2 px-4 rounded mr-2">
+        <button
+          @click="startGame('easy')"
+          class="bg-blue-500 text-white py-2 px-4 rounded mr-2"
+        >
           Easy
         </button>
-        <button @click="startGame('medium')" class="bg-blue-500 text-white py-2 px-4 rounded mr-2">
+        <button
+          @click="startGame('medium')"
+          class="bg-blue-500 text-white py-2 px-4 rounded mr-2"
+        >
           Medium
         </button>
-        <button @click="startGame('hard')" class="bg-blue-500 text-white py-2 px-4 rounded">
+        <button
+          @click="startGame('hard')"
+          class="bg-blue-500 text-white py-2 px-4 rounded"
+        >
           Hard
         </button>
       </div>
@@ -22,11 +31,17 @@
         </div>
         <div class="flex">
           <div class="grid" :class="'grid-cols-' + gridSize + ' gap-2'">
-            <div v-for="(tile, index) in tiles" :key="index" @click="() => {
-                moveTile(index);
-                playMoveSound();
-              }
-              " :class="tile === index + 1 ? correctTileStyle : normalTileStyle">
+            <div
+              v-for="(tile, index) in tiles"
+              :key="index"
+              @click="
+                () => {
+                  moveTile(index);
+                  playMoveSound();
+                }
+              "
+              :class="tile === index + 1 ? correctTileStyle : normalTileStyle"
+            >
               {{ tile === 0 ? "" : tile }}
             </div>
           </div>
@@ -34,30 +49,41 @@
         <div class="flex-2">
           <!-- add click with sound effect -->
 
-          <button @click="() => {
-              shuffle();
-              playShuffleSound();
-            }
-            " class="m-2 mt-4 bg-blue-500 text-white py-2 px-4 rounded">
+          <button
+            @click="
+              () => {
+                shuffle();
+                playShuffleSound();
+              }
+            "
+            class="m-2 mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+          >
             <img src="./components/shuffle-icon.png" alt="shuffle" />
           </button>
           <!-- add button to complete the game (โกง)-->
-          <button @click="isComplete" class="m-2 mt-4 bg-green-500 text-white py-2 px-4 rounded">
+          <button
+            @click="isComplete"
+            class="m-2 mt-4 bg-green-500 text-white py-2 px-4 rounded"
+          >
             Complete (โกง)
           </button>
-          <button @click="home" class="m-2 mt-4 bg-blue-500 text-white py-2 px-4 rounded">
-
+          <button
+            @click="home"
+            class="m-2 mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+          >
             <img src="./components/home-icon.png" alt="home" />
           </button>
-          <button id="modal" class="btn" onclick="my_modal_1.showModal()">DONE!</button>
-          <dialog id="my_modal_1" class="modal">
+
+          <!-- modal -->
+          <dialog id="my_modal_1" class="modal" v-if="isSolved">
             <div class="modal-box">
               <h3 class="font-bold text-lg">Congratulations</h3>
-              <p class="py-4">Press ESC key or click the button below to home</p>
+              <p class="py-4">
+                Press ESC key or click the button below to home
+              </p>
               <div class="modal-action">
                 <form method="dialog">
-                  <!-- if there is a button in form, it will close the modal -->
-                  <button class="btn">Close and Go Home</button>
+                  <button class="btn" @click="home">Close and Go Home</button>
                 </form>
               </div>
             </div>
@@ -98,7 +124,6 @@ let gridSize = 4; // Default grid size
 const sound1 = new Audio(shuffleSound);
 const sound2 = new Audio(moveSound);
 const sound3 = new Audio(backgroudSound);
-
 
 const startGame = (difficulty) => {
   gameStarted.value = true;
@@ -178,9 +203,9 @@ const moveTile = (index) => {
     ];
 
     if (isSolved()) {
-      setTimeout(function () {
-        alert("Congratulations");
-        gameStarted.value = false;
+      clearInterval(timerInterval);
+      setTimeout(() => {
+        my_modal_1.showModal();
       }, 1000);
     }
   }
@@ -212,7 +237,6 @@ const isSolved = () => {
       return false;
     }
   }
-
   return true;
 };
 
